da9fce875b1d33228a5b066fd9cef97ed982e4a3
Chris Queen <chris.queen2110@gmail.com>
2013-10-29 12:52:33 -0700

    Deleted unneccesary migrations

--- db/migrate/20131025222612_add_musicinfo_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddMusicinfoToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :main_instruments, :string
-    add_column :users, :other_instruments, :string
-    add_column :users, :main_genres, :string
-    add_column :users, :other_genres, :string
-    add_column :users, :links, :string
-  end
-end
