eb4a9ff8bcd08705ae4a2ac2be7c56dec239fb4c
Chris Queen <chris.queen2110@gmail.com>
2013-11-19 23:34:08 -0800

    commented migration files that removed certain items

--- db/migrate/20131028185118_remove_links_from_user.rb
+++ db/migrate/20131028185118_remove_links_from_user.rb
@@ -1,5 +1,5 @@
 class RemoveLinksFromUser < ActiveRecord::Migration
   def change
-    remove_column :users, :links, :string
+    # remove_column :users, :links, :string
   end
 end
--- db/migrate/20131028204229_remove_items_from_users_and_add_bio.rb
+++ db/migrate/20131028204229_remove_items_from_users_and_add_bio.rb
@@ -1,9 +1,9 @@
 class RemoveItemsFromUsersAndAddBio < ActiveRecord::Migration
   def change
-    remove_column :users, :main_instruments, :string
-    remove_column :users, :other_instruments, :string
-    remove_column :users, :main_genres, :string
-    remove_column :users, :other_genres, :string
+    # remove_column :users, :main_instruments, :string
+    # remove_column :users, :other_instruments, :string
+    # remove_column :users, :main_genres, :string
+    # remove_column :users, :other_genres, :string
     add_column :users, :bio, :text
   end
 end
